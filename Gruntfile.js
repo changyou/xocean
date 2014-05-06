1bbfb915bd87967d640b3b89add2e0ef9a743bb6
cxj_liang <cxj_liang@126.com>
2014-05-06 22:53:31 +0800

    kill index

--- Gruntfile.js
+++ Gruntfile.js
@@ -470,7 +470,7 @@ module.exports = function (grunt) {
       'bower-install',
       'concurrent:server',
       'autoprefixer',
-      'express:test',
+      'express:dev',
       'open',
       'watch'
     ]);
